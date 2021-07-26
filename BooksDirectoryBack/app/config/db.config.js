0d2e01d5483ab9a7ad4c6b7207e224ac9390179a
AxelBls <axel.bleuse@orange.fr>
2021-07-26 09:42:53 +0200

    Docker compose works great

--- BooksDirectoryBack/app/config/db.config.js
+++ BooksDirectoryBack/app/config/db.config.js
@@ -3,7 +3,7 @@
 const mysql = require('mysql')
 
 var con = mysql.createConnection({
-    host: "localhost",
+    host: "192.168.1.69",
     user: "root",
     password: 'root',
     database: 'booksdirectory',
